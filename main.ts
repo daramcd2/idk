026532e81856ea5062f798ea13c670a727574d9b
daramcd2 <24DMcDaniel.SPC@lmetb.ie>
2024-12-05 12:36:33 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let index = 0
+console.log("hello")
+pause(1000)
+console.log("world")
+pause(4000)
+console.log("well... want to see something cool?")
+pause(4000)
+console.log("i guess you asked for it so there")
+pause(3000)
+for (let index2 = 0; index2 < 1000; index2++) {
+    console.log("gotta go fast")
+    pause(25)
+}
+console.log("nice for an 9th grader")
+pause(1000)
+console.log("isnt it pretty cool?")
+pause(7000)
+console.log("well well well")
+pause(2000)
+console.log("here goes the lag i guess")
+pause(3000)
+for (let index2 = 0; index2 < 100000; index2++) {
+    console.log(":D")
+    pause(5)
+}
